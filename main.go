5d02994cc3e0cacff633182a3b9657254a511126
Christian Neufeld <christian.neufeld@alumni.ubc.ca>
2019-07-11 13:52:24 -0700

    added whois query and routes

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/domainr/whois"
+
+	"encoding/json"
     "log"
     "net/http"
 )
@@ -15,8 +18,59 @@ func main() {
         func(w http.ResponseWriter, r *http.Request) {
             fileServer.ServeHTTP(w, r)
         },
-    )
+	)
+	
+	// Handle the POST request on /whois
+	// client dat format: data=8.8.8.8
+	http.HandleFunc(
+		"/whois", 
+		func(w http.ResponseWriter, r *http.Request) {
+		// Verify this is POST request
+			if r.Method != "POST" {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			// Extract the encoded data to perform the whois on.
+			data := r.PostFormValue("data")
+
+			// Perform the whois query.
+			result, err := whoisQuery(data)
+
+			// Return a JSON-encoded response.
+			if err != nil {
+				jsonResponse(w, Response{Error: err.Error()})
+				return
+			}
+			jsonResponse(w, Response{Result: result})
+		},
+	)
 
     //Finally, start the HTTP server on port 8080.
     log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+type Response struct {
+    Error string `json:"error"`
+    Result string `json:"result"`
+}
+
+//Runs a who is query using github.com/domainr/whois
+func whoisQuery(data string) (string, error) {
+	response, err := whois.Fetch(data)
+	if err != nil {
+		return "", err
+	}
+	return string(response.Body), nil
+}
+func jsonResponse(w http.ResponseWriter, x interface{}) {
+    // JSON-encode x.
+    bytes, err := json.Marshal(x)
+    if err != nil {
+        panic(err)
+    }
+    // Write the encoded data to the ResponseWriter.
+    // This will send the response to the client.
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(bytes)
 }
---
+++ static/index.js
@@ -0,0 +1,23 @@
+window.onload = function(){ 
+    var form = document.getElementById('whoisInput');
+    form.onsubmit = function(e) {
+    e.preventDefault();
+    var data = document.getElementById('input').value;
+    var div = document.getElementById('resultDiv');
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/whois', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if(this.readyState !== XMLHttpRequest.DONE) {
+            return;
+        }
+        var response = JSON.parse(this.responseText);
+        if(response.error) {
+            div.innerHTML = '<p>' + response.error + '</p>';
+        } else {
+            div.innerHTML = '<pre>' + response.result + '</pre>';
+        }
+    };
+    xhr.send('data=' + encodeURIComponent(data));
+    };
+};
